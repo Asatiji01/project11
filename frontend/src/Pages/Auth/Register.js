1d2e6431fd294e93b95f45005fcecc55d6ea09fc
Devanshu Asati <prashantasati481@gmail.com>
2025-02-12 21:29:33 +0530

    Update Register.js

--- frontend/src/Pages/Auth/Register.js
+++ frontend/src/Pages/Auth/Register.js
@@ -1,7 +1,7 @@
 // SignupPage.js
 import { useCallback, useEffect, useState } from "react";
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
-import "./auth.css"; // Ensure this file is linked
+import "./auth.css";
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
@@ -11,6 +11,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import { registerUser } from '../../utils/ApiRequest';
 
 const Register = () => {
+
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
@@ -21,16 +22,19 @@ const Register = () => {
   }, [navigate]);
 
   const particlesInit = useCallback(async (engine) => {
+    // console.log(engine);
     await loadFull(engine);
   }, []);
 
   const particlesLoaded = useCallback(async (container) => {
+    // await console.log(container);
   }, []);
 
   const [values, setValues] = useState({
     name : "",
     email : "",
     password : "",
+
   });
 
   const toastOptions = {
@@ -83,16 +87,25 @@ const Register = () => {
         loaded={particlesLoaded}
         options={{
           background: {
-            color: { value: '#000' },
+            color: {
+              value: '#000',
+            },
           },
           fpsLimit: 60,
           particles: {
             number: {
               value: 200,
-              density: { enable: true, value_area: 800 },
+              density: {
+                enable: true,
+                value_area: 800,
+              },
             },
-            color: { value: '#ffcc00' },
-            shape: { type: 'circle' },
+            color: {
+              value: '#ffcc00',
+            },
+            shape: {
+              type: 'circle',
+            },
             opacity: {
               value: 0.5,
               random: true,
@@ -101,15 +114,26 @@ const Register = () => {
               value: 3,
               random: { enable: true, minimumValue: 1 },
             },
-            links: { enable: false },
-            move: { enable: true, speed: 2 },
+            links: {
+              enable: false,
+            },
+            move: {
+              enable: true,
+              speed: 2,
+            },
             life: {
               duration: {
                 sync: false,
                 value: 3,
               },
               count: 0,
-              delay: { random: { enable: true, minimumValue: 0.5 }, value: 1 },
+              delay: {
+                random: {
+                  enable: true,
+                  minimumValue: 0.5,
+                },
+                value: 1,
+              },
             },
           },
           detectRetina: true,
@@ -123,47 +147,51 @@ const Register = () => {
           bottom: 0,
         }}
       />
-      <Container className="mt-5 slide-in-container" style={{ position: 'relative', zIndex: 2, color: 'black', backgroundColor: 'white', width: '50vw', height: '90vh', borderRadius: '30px' }}>
-        <Row>
-          <h1 className="text-center">
-            <AccountBalanceWalletIcon sx={{ fontSize: 40, color: "black" }} className="text-center" />
-          </h1>
-          <h1 className="text-center text-black">Welcome to Explore And Track Your Expenses</h1>
-          <Col md={{ span: 6, offset: 3 }}>
-            <h2 className="text-black text-center mt-5">Registration</h2>
-            <Form>
-              <Form.Group controlId="formBasicName" className="mt-3">
-                <Form.Label className="text-black">Name</Form.Label>
-                <Form.Control type="text" name="name" placeholder="Full name" value={values.name} onChange={handleChange} />
-              </Form.Group>
-              <Form.Group controlId="formBasicEmail" className="mt-3">
-                <Form.Label className="text-black">Email address</Form.Label>
-                <Form.Control type="email" name="email" placeholder="Enter email" value={values.email} onChange={handleChange} />
-              </Form.Group>
-              <Form.Group controlId="formBasicPassword" className="mt-3">
-                <Form.Label className="text-black">Password</Form.Label>
-                <Form.Control type="password" name="password" placeholder="Password" value={values.password} onChange={handleChange} />
-              </Form.Group>
-              <div style={{ width: "100%", display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column" }} className="mt-4">
-                <Link to="/forgotPassword" className="text-black lnk">Forgot Password?</Link>
-                <Button
+
+      <Container className="mt-5" style={{position: 'relative', zIndex: "2 !important", color:"white !important"}}>
+      <Row>
+        <h1 className="text-center">
+          <AccountBalanceWalletIcon sx={{ fontSize: 40, color: "white"}}  className="text-center" />
+        </h1>
+        <h1 className="text-center text-white">Welcome to Expense Management System</h1>
+        <Col md={{ span: 6, offset: 3 }}>
+          <h2 className="text-white text-center mt-5" >Registration</h2>
+          <Form>
+            <Form.Group controlId="formBasicName" className="mt-3" >
+              <Form.Label className="text-white">Name</Form.Label>
+              <Form.Control type="text"  name="name" placeholder="Full name" value={values.name} onChange={handleChange} />
+            </Form.Group>
+            <Form.Group controlId="formBasicEmail" className="mt-3">
+              <Form.Label className="text-white">Email address</Form.Label>
+              <Form.Control type="email"  name="email" placeholder="Enter email" value={values.email} onChange={handleChange}/>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword" className="mt-3">
+              <Form.Label className="text-white">Password</Form.Label>
+              <Form.Control type="password"  name="password" placeholder="Password" value={values.password} onChange={handleChange} />
+            </Form.Group>
+            <div style={{width: "100%", display: "flex" , alignItems:"center", justifyContent:"center", flexDirection: "column"}} className="mt-4">
+              <Link to="/forgotPassword" className="text-white lnk" >Forgot Password?</Link>
+
+              <Button
                   type="submit"
-                  className="text-center mt-3 btnStyle"
+                  className=" text-center mt-3 btnStyle"
                   onClick={!loading ? handleSubmit : null}
                   disabled={loading}
                 >
                   {loading ? "Registering..." : "Signup"}
                 </Button>
-                <p className="mt-3" style={{ color: "#9d9494" }}>Already have an account? <Link to="/login" className="text-black lnk">Login</Link></p>
-              </div>
-            </Form>
-          </Col>
-        </Row>
-        <ToastContainer />
-      </Container>
+
+              <p className="mt-3" style={{color: "#9d9494"}}>Already have an account? <Link to="/login" className="text-white lnk" >Login</Link></p>
+            </div>
+          </Form>
+        </Col>
+      </Row>
+    <ToastContainer />
+    </Container>
     </div>
     </>
-  );
-};
+  )
+}
 
-export default Register;
+export default Register
